896d8e57b093e0b04d4b1a3ad08bc772bc4df287
Grzegorz Smoktunowicz <biarth@wp.pl>
2022-08-23 16:53:56 +0200

    new commit

--- client/src/components/axios.js
+++ client/src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://mern-cryptoapp.herokuapp.com",
 });
 
 export const apiCoins = () =>
